42cf844ff8169190f9d08d53bc33482d0cc9deb1
HSH-1616 <hgfgf1116@gmail.com>
2023-09-15 00:00:07 +0900

    LV1 달리기 경주 문제풀이 2023-09-14

---
+++ src/com/programmers/level/one/RunningTest.java
@@ -0,0 +1,71 @@
+package com.programmers.level.one;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class RunningTest {
+	    public String[] solution(String[] players, String[] callings) {
+	    	//입출력 예
+			players = new String[]{ "mumu", "soe", "poe", "kai", "mine" };
+			callings = new String[]{ "kai", "kai", "mine", "mine" };
+	    		    	
+	    	String[] answer = {};
+
+			//players배열 map으로 작성
+			Map<String, Integer> player = new HashMap<>();
+			//추월당한 사람을 구하는 map
+			Map<Integer, String> player2 = new HashMap<>();
+
+			for (int i = 0; i < players.length; i++) {
+				player.put(players[i], i);
+				player2.put(i, players[i]);
+			}
+
+			for (int i = 0; i < callings.length; i++) {
+				
+				//추월한 선수 순위
+				int rank = player.get(callings[i]);				
+				//rank-1로 추월당한 선수 이름 구하기
+				String name = player2.get(rank - 1);
+				
+				//추월한 선수를 앞으로
+				player.put(callings[i], rank - 1);
+				//추월당한 선수를 뒤로
+				player.put(name, rank);
+				
+				//추월당한 선수를 구하기 위해 player2도 정렬
+				player2.put(rank - 1, callings[i]);
+				player2.put(rank, name);
+			}
+			
+			//Collection을 사용해서 map을 배열으로
+			Collection<String> result = player2.values();
+			answer = result.toArray(new String[0]);
+            
+	        return answer;        
+	    }	    
+	}
+	
+//  시간초과로 인한 오류 풀이
+//	class Solution {
+//	    public String[] solution(String[] players, String[] callings) {
+//	        String[] answer = {};        
+//	        for(int i=0;i<callings.length;i++){            
+//	            for(int j=0;j<players.length;j++){
+//	                if(players[j].equals(callings[i])){
+//	                    String name=players[j-1];
+//	                    players[j-1]=players[j];
+//	                    players[j]=name;                    
+//	                }
+//	            }
+//	        }     
+//	        answer=players;                
+//	        return answer;        
+//	    }
+//	    
+//	}
+
+
---
+++ src/com/programmers/operator/Operator.java
@@ -0,0 +1,17 @@
+package com.programmers.operator;
+
+import java.util.Arrays;
+
+import com.programmers.level.one.*;
+
+public class Operator {
+	public static void main(String[] args) {
+		
+		//Level 1		
+		
+		//달리기 경주 2023-09-14
+		String[] running=new RunningTest().solution(args, args); System.out.println(Arrays.toString(running));
+		
+	}
+	
+}
